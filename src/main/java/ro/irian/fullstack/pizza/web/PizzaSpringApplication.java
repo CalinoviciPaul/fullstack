39ef028836f3f0b0e7f24461b2bec2f19f91e565
Cristi Toth <cristi.toth@irian.ro>
2017-05-18 03:02:41 +0300

    Spring REST - Example 01

---
+++ src/main/java/ro/irian/fullstack/pizza/web/PizzaSpringApplication.java
@@ -0,0 +1,15 @@
+package ro.irian.fullstack.pizza.web;
+
+/**
+ * Spring boot main configuration
+ *
+ * @author Cristi Toth
+ */
+
+public class PizzaSpringApplication {
+
+    public static void main(String[] args) {
+        //TODO
+    }
+
+}
